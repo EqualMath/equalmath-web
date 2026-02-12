57ff3a22ca485f19eb0f54c07499ad10c9d1bd7d
Aiden Fong <afong413@gmail.com>
2026-02-12 14:26:17 -0800

    Add 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="flex min-h-full flex-1 items-center justify-center">
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-9xl font-bold">404</h1>
+        <p className="text-3xl font-bold">Page Not Found</p>
+      </div>
+    </div>
+  )
+}
