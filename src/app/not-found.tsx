71282eaea8716722833deec1a04c52541019bb6d
Aiden Fong <afong413@gmail.com>
2026-02-16 16:58:09 -0800

    Center 404 page using absolute

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ import type { JSX } from "react"
 
 export default function NotFound(): JSX.Element {
   return (
-    <div className="flex min-h-full flex-1 items-center justify-center">
+    <div className="absolute top-1/2 left-1/2 flex -translate-x-1/2 -translate-y-1/2 items-center justify-center">
       <div className="flex flex-col items-center justify-center">
         <h1 className="text-9xl font-bold">404</h1>
         <p className="text-3xl font-bold">Page Not Found</p>
