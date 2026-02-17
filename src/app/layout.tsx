8f253866a536ff1439bf4878a03f86e51b736bf0
Aiden Fong <afong413@gmail.com>
2026-02-16 16:57:33 -0800

    Add viewport meta tag and main to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,9 @@ export default function RootLayout({
 }>): JSX.Element {
   return (
     <html lang="en" className="flex min-h-full flex-col">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body
         className={cn(
           geistSans.variable,
@@ -37,7 +40,7 @@ export default function RootLayout({
         )}
       >
         <NavigationBar />
-        {children}
+        <main className="flex min-h-full flex-1 flex-col">{children}</main>
       </body>
     </html>
   )
