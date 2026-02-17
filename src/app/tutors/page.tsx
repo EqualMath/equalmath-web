1d6f52d5a0e9df0cdfff4a7461790881d2e99e64
Aiden Fong <afong413@gmail.com>
2026-02-16 17:10:41 -0800

    Reduce to tutors list to one column on smaller screens

--- src/app/tutors/page.tsx
+++ src/app/tutors/page.tsx
@@ -7,7 +7,7 @@ export default async function Tutors() {
   return (
     <div className="bg-background flex min-h-full flex-1 flex-col gap-8 px-48 py-8">
       <h1 className="text-4xl font-semibold">Tutors</h1>
-      <div className="grid grid-cols-2 gap-8">
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-2">
         {tutors.map((tutor, i) => (
           <TutorItem tutor={tutor} key={i} variant="outline" />
         ))}
